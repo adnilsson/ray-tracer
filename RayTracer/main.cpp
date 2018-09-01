cac85064c2f17cfde6d43b47c9df49d0b9da1f3a
Adrian Nilsson <adnilsson@users.noreply.github.com>
2018-09-01 13:31:40 +0200

    Ch4 code. Add normalize_direction() for rays.

--- RayTracer/main.cpp
+++ RayTracer/main.cpp
@@ -1,8 +1,9 @@
 /**
 * Ordering of includes matters.
-* stb_image_write.h must be included first, otherwise the
+* stb_image_write.h must be included first of all, otherwise
 * define _CRT_SECURE_NO_WARNINGS does not go through and VS will
 * build with errors saying that fopen may be unsafe.
+* I do not know what causes this specific behaviour.
 **/
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
@@ -12,22 +13,42 @@
 
 #ifndef EIGENH
 #define EIGENH
-	#include <Eigen/Core>
-	using Eigen::Vector3f;
+#include <Eigen/Core>
+using Eigen::Vector3f;
 #endif 
 
-#include <iostream>
 #include "ray.h"
 
 typedef uint8_t byte;
 
 /**
+* Follows the test described by Real Time Rendering 3rd-edition.
+* For now, this does not compute the points of intersection.
+* Assumes that Ray r has a normalized direction vector.
+* TODO: change to the optimized test.
+*/
+bool intersects_shpere(const Vector3f& center, float radius, const Ray &r) {
+	Vector3f oc = r.origin() - center;
+
+	float c = oc.dot(oc) - pow(radius, 2);
+	float b = r.direction().dot(oc);
+	float discriminant = pow(b, 2) - c;
+
+	// Gives true if distcriminant = 0.0f
+	return (discriminant + FLT_EPSILON > 0.0f );
+}
+
+
+/**
 * Gives a color that is linerarly interpolated between white and 
 * blue-ish based on the direction of the ray.
+* Assumes normalized Ray direction vector.
 **/
-Vector3f color(const ray &r) {
-	Vector3f unit_direction = r.direction().normalized();
-	float t = 0.5f*(unit_direction.y() + 1.0f);
+Vector3f color(const Ray &r) {
+	if (intersects_shpere(Vector3f(0, 0, -1), 0.5f, r)) {
+		return Vector3f(1, 0, 0);
+	}
+	float t = 0.5f*(r.direction().y() + 1.0f);
 	return (1.0f - t)*Vector3f(1.0f, 1.0f, 1.0f) + t * Vector3f(0.5f, 0.7f, 1.0f);
 }
 
@@ -50,9 +71,10 @@ int main() {
 		for (int ix = 0; ix < nx; ix++) {
 			float u = float(ix) / float(nx);
 			float v = float(ny - iy - 1) / float(ny);
-			ray r(origin, lower_left_corner + u*horizontal + v*vertical);
-			
-			Vector3f c = color(r) * 255.99f; // Offending line
+			Ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+			r.normalize_direction();
+
+			Vector3f c = color(r) * 255.99f;
 			
 			rgb_image[i] = byte(c.x());
 			rgb_image[i + 1] = byte(c.y());
@@ -60,7 +82,7 @@ int main() {
 			i += RGB_CHANNELS;
 		}
 	}
-	stbi_write_png("ch3.png", nx, ny, RGB_CHANNELS, rgb_image, 0);
+	stbi_write_png("ch4.png", nx, ny, RGB_CHANNELS, rgb_image, 0);
 
 	delete[] rgb_image;
 }
--- RayTracer/ray.h
+++ RayTracer/ray.h
@@ -7,18 +7,20 @@
 #endif
 
 
-class ray {
+class Ray {
 private:
 	Eigen::Vector3f A, B;
 public:
 	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-	ray() {};
-	ray(const Eigen::Vector3f& a, const Eigen::Vector3f& b) { A = a; B = b; }
+	Ray() {};
+	Ray(const Eigen::Vector3f& a, const Eigen::Vector3f& b) { A = a; B = b; }
 
 	Eigen::Vector3f origin() const		{ return A; }
 	Eigen::Vector3f direction() const	{ return B; }
 	Eigen::Vector3f point_at_parameter(float t) const { return A + B * t; }
+
+	void normalize_direction() { B.normalize(); }
 
 };
 
