de942cec00f90c31686d5d75971662d60de5e4fd
Adrian Nilsson <adnilsson@users.noreply.github.com>
2019-04-20 22:38:59 +0200

    1080p render after issues #1 and #2

--- RayTracer/main.cpp
+++ RayTracer/main.cpp
@@ -82,9 +82,9 @@ Hitable *random_scene() {
 
 int main() {
   const int RGB_CHANNELS = 3;
-  const int nx = 600;
-  const int ny = 400;
-  const int ns = 100;
+  const int nx = 1920;
+  const int ny = 1080;
+  const int ns = 1000;
   byte *rgb_image = new byte[nx*ny*RGB_CHANNELS];
 
 
