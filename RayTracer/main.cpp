2b9f18ccc565f9b0420198a3802cb21955bf0cde
Adrian Nilsson <adnilsson@users.noreply.github.com>
2019-04-24 22:27:06 +0200

    Trace rows of x-pixels in parallel
    The loop over x-pixlels is replaced with a parallel for_each. This means that a row of pixels are traced in a parallel, but each row is sequential. The current approach does all work first and updates the image afterwards, which means that the order of execution matters.
    
    Work:  O(ns)*O(|world|). Tracing ns rays up to a depth of 50 and reduce to a final pixel color.
    Depth: O(nx) size of a pixel-row.

--- RayTracer/main.cpp
+++ RayTracer/main.cpp
@@ -24,16 +24,26 @@ using Eigen::Vector3f;
 #include "Material.h"
 #include "utils.h"
 
+#include <array>
+#include <functional>
+#include <numeric>
+#include <execution>
+
+
 typedef uint8_t byte;
 #define T_MIN utils::EPSILON
+
+constexpr int RGB_CHANNELS = 3;
+constexpr int ns           = 10;
+
 
 /**
 * Recursively trace rays with the geometry until background is hit.
 * Assumes normalized Ray direction vector.
 **/
 Vector3f color(const Ray &r, Hitable *world, int depth) {
-	hit_record rec;
-	if (world -> hit(r, T_MIN, FLT_MAX, rec)) {
+  hit_record rec;
+  if (world->hit(r, T_MIN, FLT_MAX, rec)) {
     Ray scatter_ray;
     Vector3f attenuation;
     if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scatter_ray)) {
@@ -41,19 +51,19 @@ Vector3f color(const Ray &r, Hitable *world, int depth) {
     }
 
     return Vector3f(0, 0, 0);
-	}
+  }
 
-	//No intersection. Give background color instead.
-	float t = 0.5f*(r.direction().y() + 1.0f);
-	return (1.0f - t)*Vector3f(1.0f, 1.0f, 1.0f) + t * Vector3f(0.5f, 0.7f, 1.0f);
+  //No intersection. Give background color instead.
+  float t = 0.5f*(r.direction().y() + 1.0f);
+  return (1.0f - t)*Vector3f(1.0f, 1.0f, 1.0f) + t * Vector3f(0.5f, 0.7f, 1.0f);
 }
 
 
 Hitable *random_scene() {
   int n = 500;
-  Hitable **list = new Hitable*[n+1];
-  list[0] = new Sphere(Vector3f(0,-1000,0), 1000, new Lambertian(Vector3f(0.5, 0.5, 0.5)));
-  
+  Hitable **list = new Hitable*[n + 1];
+  list[0] = new Sphere(Vector3f(0, -1000, 0), 1000, new Lambertian(Vector3f(0.5, 0.5, 0.5)));
+
   int i = 1;
   for (int a = -11; a < 11; a++) {
     for (int b = -11; b < 11; b++) {
@@ -80,49 +90,72 @@ Hitable *random_scene() {
   return new HitableList(list, i);
 }
 
+Ray sample_ray(const Camera &cam, const int nx, const int ny, const int ix, const int iy) {
+  float u = (float(ix) + utils::randf()) / float(nx);
+  float v = (float(ny - iy - 1) + utils::randf()) / float(ny);
+  Ray r = cam.get_ray(u, v);
+  r.normalize_direction();
+  return r;
+}
+
+Vector3f pixel_color(const Camera &cam, Hitable *world, int nx, int ny, int ix, int iy) {
+  Vector3f final_color(0, 0, 0);
+  for (int is = 0; is < ns; is++) {
+    Ray r = sample_ray(cam, nx, ny, ix, iy);
+    final_color += color(r, world, 0);
+  }
+  final_color /= float(ns);
+  final_color = Vector3f(sqrt(final_color.x()) * 255.99,
+                         sqrt(final_color.y()) * 255.99,
+                         sqrt(final_color.z()) * 255.99);
+  return final_color;
+}
+
 int main() {
-  const int RGB_CHANNELS = 3;
-  const int nx = 1920;
-  const int ny = 1080;
-  const int ns = 1000;
+  const int nx = 600;
+  const int ny = 300;
+
   byte *rgb_image = new byte[nx*ny*RGB_CHANNELS];
 
 
   Hitable *world = random_scene();
 
-  Vector3f lookfrom   = Vector3f(13.0f, 2.0f, 3.0f);
-  Vector3f lookat     = Vector3f(0.0f, 0.0f, 0.0f);
+  Vector3f lookfrom = Vector3f(13.0f, 2.0f, 3.0f);
+  Vector3f lookat = Vector3f(0.0f, 0.0f, 0.0f);
   float dist_to_focus = 10.0f;
-  float aperture      = 0.1f;
-  float vfov          = 20.0f;
-	Camera cam(lookfrom, lookat, Vector3f(0.0f, 1.0f, 0.0f), vfov
-            , static_cast<float>(nx)/ static_cast<float>(ny)
-            , aperture, dist_to_focus);
+  float aperture = 0.1f;
+  float vfov = 20.0f;
+  Camera cam(lookfrom, lookat, Vector3f(0.0f, 1.0f, 0.0f), vfov
+    , static_cast<float>(nx) / static_cast<float>(ny)
+    , aperture, dist_to_focus);
 
-	int i = 0;
-	for (int iy = 0; iy < ny; iy++) {
-		for (int ix = 0; ix < nx; ix++) {
-			Vector3f c(0, 0, 0);
-			for (int is = 0; is < ns ; is++) {
-				float u = (float(ix) + utils::randf()) / float(nx);
-				float v = (float(ny - iy - 1) + utils::randf()) / float(ny);
-				Ray r = cam.get_ray(u, v);
-				r.normalize_direction();
+  int i = 0;
+  for (int iy = 0; iy < ny; iy++) {
+    //enumerate this pixel row
+    std::array<int, nx> all_x; 
+    std::iota(all_x.begin(), all_x.end(), 0);
+    
+    //colors for each pixel
+    std::array<Vector3f, nx> all_colors; 
+    auto tracer = std::bind(pixel_color, cam, world, nx, ny, std::placeholders::_1, iy);
+    std::for_each(std::execution::par,             //for_each guarantees in-order execution
+                  all_x.begin(), all_x.end(), 
+                  [tracer, &all_colors](auto i) {  //update all_colors as a side effect
+                      all_colors[i] = tracer(i);
+                  });
 
-				c += color(r, world, 0);
-			}
-			
-			c /= float(ns);
-			rgb_image[i] = byte(sqrt(c.x()) * 255.99);
-			rgb_image[i + 1] = byte(sqrt(c.y()) * 255.99);
-			rgb_image[i + 2] = byte(sqrt(c.z()) * 255.99);
-			i += RGB_CHANNELS;
-		}
-	}
-	stbi_write_png("ch12.png", nx, ny, RGB_CHANNELS, rgb_image, 0);
+    //record traced colors
+    for (auto pixel : all_colors) {
+      rgb_image[i]     = byte(pixel.x());
+      rgb_image[i + 1] = byte(pixel.y());
+      rgb_image[i + 2] = byte(pixel.z());
+      i += RGB_CHANNELS;
+    }
+  }
+  stbi_write_png("ch12.png", nx, ny, RGB_CHANNELS, rgb_image, 0);
 
 
-	// de-allocation 
-	delete[] rgb_image;
-	delete world; //owns its list of Hitables and deletes them in its destructor
+  // de-allocation 
+  delete[] rgb_image;
+  delete world; //owns its list of Hitables and deletes them in its destructor
 }
