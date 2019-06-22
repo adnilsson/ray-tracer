a2c0475620662c5fc7cd80b62897efa41202376b
Adrian Nilsson <adnilsson@users.noreply.github.com>
2019-06-22 22:29:37 +0200

    Shpere: Use an optimized intersection test
    This more than halved the time it took to render the radomized scene on my Ryzen 1600.

--- RayTracer/Sphere.cpp
+++ RayTracer/Sphere.cpp
@@ -4,36 +4,45 @@
 * Follows the test described by Real Time Rendering 3rd-edition.
 * Assumes that Ray r has a normalized direction vector.
 * Side-effect: places the most positive t in isect_t if intersection occurs.
-* TODO: change to the optimized test.
+*
+* Returns true if the ray intersects the sphere and records intersection info
+* (vector to point of intersetion, material) in rec.
 */
 bool Sphere::hit(const Ray &r, float t_min, float t_max, hit_record &rec) const {
-	Eigen::Vector3f oc = r.origin() - center;
+	Eigen::Vector3f oc =  center - r.origin();
+  
+  float s  = oc.dot(r.direction());
+  float oc_squared_len = oc.dot(oc);
+  float radius_sqaured = radius * radius;
 
-	float c = oc.dot(oc) - pow(radius, 2);
-	float b = r.direction().dot(oc);
-	float discriminant = pow(b, 2) - c;
+  // Reject if ray origin is behind the sphere center and 
+  // more than a radius distance away
+  if (s < 0.0f && oc_squared_len > radius_sqaured) {
+    return false;
+  }
 
-	// Gives true if distcriminant = 0.0f
-	if (discriminant + FLT_EPSILON > 0.0f) {
-		float inv_rad = 1 / radius;
-		
-		float temp = -b - sqrt(discriminant);
-		if (temp < t_max && temp > t_min) {
-			rec.t = temp;
-			rec.p = r.point_at_parameter(temp);
-			rec.normal = (rec.p - center) *inv_rad;
-      rec.mat_ptr = mat_ptr;
-			return true;
-		}
+  // Reject if the ray will miss the sphere
+  float m_squared = oc_squared_len - s * s;
+  if (m_squared > radius_sqaured) return false;
 
-		temp = -b + sqrt(discriminant);
-		if (temp < t_max && temp > t_min) {
-			rec.t = temp;
-			rec.p = r.point_at_parameter(temp);
-			rec.normal = (rec.p - center) *inv_rad;
-      rec.mat_ptr = mat_ptr;
-			return true;
-		}
-	}
-	return false;
+  // We now know that the ray intersects the sphere.
+
+  float q = sqrt(radius_sqaured - m_squared);
+  float t;
+  if (oc_squared_len > radius_sqaured) {
+    t = s - q;
+  }
+  else {
+    t = s + q;
+  }
+
+  if (t < t_min || t > t_max) {
+    return false;
+  }
+
+  rec.t = t;
+  rec.p = r.point_at_parameter(t);
+  rec.normal = (rec.p - center) / radius;
+  rec.mat_ptr = mat_ptr;
+  return true;
 }
